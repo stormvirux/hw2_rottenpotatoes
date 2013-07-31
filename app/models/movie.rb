3d4a66d73f0708d9845391c5ec3e64c0c6ec58e9
stormvirux <mthaha1989@gmail.com>
2013-07-31 14:45:54 +0300

    lol

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,11 @@
 class Movie < ActiveRecord::Base
   def self.filtered(params)
     movies = Movie
-    movies=movies.order("#{params[:sort]}  ASC")
+    if params[:sort]=="title"
+	movies=movies.order("title  ASC")
+	else
+	movies=movies.order("release_date  ASC")
+	end
 	movies.all
   end
 end
